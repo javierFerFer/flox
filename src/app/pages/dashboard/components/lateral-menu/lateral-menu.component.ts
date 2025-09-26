8b68c42a200a6817c3e1c7a6208ebecdabe9028e
Javier Fernandez <javierferfer99@gmail.com>
2025-09-26 15:21:12 +0000

    refactor(): converted lateral and topbar menus into components + fix related with routerLink

--- src/app/pages/dashboard/components/lateral-menu/lateral-menu.component.ts
+++ src/app/pages/dashboard/components/lateral-menu/lateral-menu.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-lateral-menu',
-  imports: [MenuModule],
+  imports: [MenuModule, RouterModule],
   templateUrl: 'lateral-menu.component.html',
   styleUrl: 'lateral-menu.component.scss',
   standalone: true,
 })
 export class LateralMenuComponent {
+  constructor(private router: Router) {}
+
   // continuar aca
-  model = [
+  model: MenuItem[] = [
     {
-      icon: 'pi pi-user',
-      label: 'test me',
-      routerLink: ['/dashboard/records'],
+      icon: 'pi pi-home',
+      // route: '/dashboard/records',
+      routerLink: '/dashboard/records',
+      // url: '/dashboard/records',
+      // opcionalmente:
+      // routerLinkActiveOptions: { exact: false },
+      // command: () => {
+      //   console.log('hola');
+      //   this.router.navigate(['installation']);
+      // },
     },
-    { icon: 'pi pi-search', routerLink: ['/dashboard/recordsasdasd'] },
+    // etc.
   ];
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,35 +1,30 @@
 import { Component, inject } from '@angular/core';
-import { UserStore } from '../../stores/user/user.store';
-import { doc, Firestore } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
 import { ButtonModule } from 'primeng/button';
 import { Auth, signOut } from '@angular/fire/auth';
-import { ToolbarModule } from 'primeng/toolbar';
-import { AvatarModule } from 'primeng/avatar';
-import { AvatarGroupModule } from 'primeng/avatargroup';
+
 import { RouterModule } from '@angular/router';
 import { LateralMenuComponent } from './components/lateral-menu/lateral-menu.component';
+import { CustomToolbarComponent } from './components/custom-toolbar/custom-toolbar.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     ButtonModule,
-    ToolbarModule,
-    AvatarModule,
-    AvatarGroupModule,
     RouterModule,
     LateralMenuComponent,
+    CustomToolbarComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  userStore = inject(UserStore);
   private readonly firestore = inject(Firestore);
   private readonly auth = inject(Auth);
-  private readonly userDoc = doc(
-    this.firestore,
-    `users/${this.userStore.user().uid}`,
-  );
+  // private readonly userDoc = doc(
+  //   this.firestore,
+  //   `users/${this.userStore.user().uid}`,
+  // );
 
   constructor() {
     // setTimeout(() => {
