6a3711afb6a61f94ba963028cf882cd4ddfa63f7
Javier Fernandez <javierferfer99@gmail.com>
2025-11-05 13:26:30 +0100

    fix(notification-version): solved problem related with how to read the version of the app works

--- src/app/resolvers/new-updates.resolver.ts
+++ src/app/resolvers/new-updates.resolver.ts
@@ -1,7 +1,10 @@
 import { inject, Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap, take } from 'rxjs';
 import { ProjectVersionService } from '../services/project-version/project-version.service';
+import { UserStore } from '../stores/user/user.store';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { PROJECT_VERSION } from '../version.config';
 
 export type UserTheme = 'light' | 'dark';
 
@@ -12,9 +15,22 @@ export interface FirebaseNewUpdates {
 
 @Injectable({ providedIn: 'root' })
 export class NewUpdatesResolver implements Resolve<any> {
+  private readonly userStore = inject(UserStore);
   private readonly projectVersionService = inject(ProjectVersionService);
+  private readonly version = inject(PROJECT_VERSION).version;
+  private userObs;
+  constructor() {
+    this.userObs = toObservable(this.userStore.user).pipe(
+      take(1),
+      switchMap((user) => {
+        return user.userConfig?.lastVersionReadIt !== this.version
+          ? this.projectVersionService.getAppInfo()
+          : of(undefined);
+      }),
+    );
+  }
 
   resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> | any {
-    return this.projectVersionService.getAppInfo();
+    return this.userObs;
   }
 }
