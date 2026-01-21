f83824b7c85849bc7f5b2f77706da50ab1ed3794
Javier Fernández Fernández <javierferfer99@gmail.com>
2026-01-21 16:30:31 +0100

    chore(): bump version to 0.35.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -80,7 +80,7 @@ export const appConfig: ApplicationConfig = {
     }),
     {
       provide: PROJECT_VERSION,
-      useValue: { version: '0.34.3' },
+      useValue: { version: '0.35.0' },
     },
   ],
 };
