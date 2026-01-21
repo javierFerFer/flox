650b16a040b7fcb273cdb8d8d87b2907440e7df8
Javier Fernández Fernández <javierferfer99@gmail.com>
2026-01-21 13:59:37 +0100

    chore(): removed console log

--- src/app/directives/no-suggest.directive.ts
+++ src/app/directives/no-suggest.directive.ts
@@ -7,7 +7,6 @@ export class NoSuggestDirective implements OnInit {
   private readonly userStore = inject(UserStore);
 
   ngOnInit(): void {
-    console.log('test', this.userStore.user().userConfig?.suggestInputs)
     if (!this.userStore.user().userConfig?.suggestInputs) {
       (this.elementRef.nativeElement as HTMLInputElement).autocomplete = 'off';
     } else {
